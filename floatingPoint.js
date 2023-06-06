9c79a9514210c2550d5d485a585ba347e7719d39
sami12344 <freelancersami16203@gmail.com>
2023-06-06 13:18:58 +0600

    Floating Point

---
+++ floatingPoint.js
@@ -0,0 +1,5 @@
+let x = 0.3 - 0.2 // thirty cents minus 20 cents
+let y = 0.2 - 0.1 // twenty cents minus 10 cents
+x === y // => false: the two values are not the same!
+x === 0.1 // => false: .3-.2 is not equal to .1
+y === 0.1 // => true: .2-.1 is equal to .1
