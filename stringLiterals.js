a15ea3cb50d7c7c9493817d71772e5a9693ef590
sami12344 <freelancersami16203@gmail.com>
2023-06-06 13:36:26 +0600

    String Literals

---
+++ stringLiterals.js
@@ -0,0 +1,6 @@
+'' // The empty string: it has zero characters
+'testing'
+'3.14'
+'name="myform"'
+"Wouldn't you prefer O'Reilly's book?"
+"τ is the ratio of a circle's circumference to its radius"`"She said 'hi'", he said.`
