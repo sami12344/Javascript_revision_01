b1bf0eb3295ec42e8a96c80a985201a4d8e79aca
sami12344 <freelancersami16203@gmail.com>
2023-06-06 10:23:03 +0600

    literal

---
+++ literals.js
@@ -0,0 +1,10 @@
+/*A literal is a data value that appears directly in a program. The following are all
+literals:
+*/
+12 // The number twelve
+1.2 // The number one point two
+"hello world" // A string of text
+'Hi' // Another string
+true // A Boolean value
+false // The other Boolean value
+null // Absence of an object
