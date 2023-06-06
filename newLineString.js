d8f561684087b601308fbf16ef010946f47036ca
sami12344 <freelancersami16203@gmail.com>
2023-06-06 13:40:17 +0600

    New Line String

---
+++ newLineString.js
@@ -0,0 +1,8 @@
+// A string representing 2 lines written on one line:
+'two\nlines'
+// A one-line string written on 3 lines:
+'one\
+ long\
+ line'// A two-line string written on two lines:
+`the newline character at the end of this line
+is included literally in this string`
