2a26a446b4ed9f697188452e4bf732d02cad46c9
sami12344 <freelancersami16203@gmail.com>
2023-06-06 12:30:18 +0600

    Hexadecimal Literals

---
+++ hexadecimalLiteral.js
@@ -0,0 +1,6 @@
+0xff // => 255: (15*16 + 15)
+0xBADCAFE // => 195939070
+// In ES6 and later, you can also express integers in binary (base 2) or octal (base 8)
+// using the prefixes 0b and 0o (or 0B and 0O) instead of 0x:
+0b10101 // => 21: (1*16 + 0*8 + 1*4 + 0*2 + 1*1)
+0o377 // => 255: (3*64 + 7*8 + 7*1)
