b35fd8756c963ef8dfe6a6943c765f61c285caf9
sami12344 <freelancersami16203@gmail.com>
2023-06-06 10:28:04 +0600

    Reserved Words

---
+++ reserverdWords.js
@@ -0,0 +1,17 @@
+/* as const export get null target void
+async continue extends if of this while
+await debugger false import return throw with
+break default finally in set true yield
+case delete for instanceof static try
+catch do from let super typeof
+class else function new switch var
+
+JavaScript also reserves or restricts the use of certain keywords that are not currently
+used by the language but that might be used in future versions:
+
+enum implements interface package private protected public
+
+For historical reasons, arguments and eval are not allowed as identifiers in certain
+circumstances and are best avoided entirely.
+
+*/
