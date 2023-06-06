d856feeb13509c29708092def98b91cd71a0ede2
sami12344 <freelancersami16203@gmail.com>
2023-06-06 13:31:12 +0600

    Date and Time

---
+++ dateAndTimes.js
@@ -0,0 +1,4 @@
+let timestamp = Date.now() // The current time as a timestamp (a number).
+let now = new Date() // The current time as a Date object.
+let ms = now.getTime() // Convert to a millisecond timestamp.
+let iso = now.toISOString() // Convert to a string in standard format.
