78bc52e7595af99de3852a16c1577010056c6612
sami12344 <freelancersami16203@gmail.com>
2023-06-06 13:52:16 +0600

    JavaScript Escape Sequence

---
+++ escapeSequence.js
@@ -0,0 +1,19 @@
+/*
+
+Sequence Character represented
+\0 The NUL character (\u0000)
+\b Backspace (\u0008)
+\t Horizontal tab (\u0009)
+\n Newline (\u000A)
+\v Vertical tab (\u000B)
+\f Form feed (\u000C)
+\r Carriage return (\u000D)
+\" Double quote (\u0022)
+\' Apostrophe or single quote (\u0027)
+\\ Backslash (\u005C)
+\xnn The Unicode character specied by the two hexadecimal digits nn
+\unnnn The Unicode character specied by the four hexadecimal digits nnnn
+\u{n} The Unicode character specied by the codepoint n, where n is one to six hexadecimal digits between 0 and
+10FFFF (ES6)
+
+*/
