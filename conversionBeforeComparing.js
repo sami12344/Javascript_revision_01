7b0d93f81ecb1ff94240d12f334f674d61806cd3
sami12344 <freelancersami16203@gmail.com>
2023-06-09 11:21:51 +0600

    Conversion before Comparing

---
+++ conversionBeforeComparing.js
@@ -0,0 +1,4 @@
+null == undefined // => true: These two values are treated as equal.
+'0' == 0 // => true: String converts to a number before comparing.
+0 == false // => true: Boolean converts to number before comparing.
+'0' == false // => true: Both operands convert to 0 before comparing!
